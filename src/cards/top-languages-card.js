8017f8ee2f15d5f8cf375d5796a2b013ff50a524
VinayR-GitHub <153238516+VinayR-GitHub@users.noreply.github.com>
2024-01-02 14:51:18 +1100

    Update top-languages-card.js

--- src/cards/top-languages-card.js
+++ src/cards/top-languages-card.js
@@ -110,7 +110,7 @@ const getCircleLength = (radius) => {
  * @returns {number} Card height.
  */
 const calculateCompactLayoutHeight = (totalLangs) => {
-  return COMPACT_LAYOUT_BASE_HEIGHT + Math.round(totalLangs / 4) * 25;
+  return COMPACT_LAYOUT_BASE_HEIGHT + Math.round(totalLangs / 2) * 25;
 };
 
 /**
